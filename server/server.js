b12ac608bdc206193006216192ac0e35342a028f
Muraydevs1 <139182142+Muraydevs1@users.noreply.github.com>
2026-06-21 02:24:15 +0000

    created server for code explainer... calls the openai llm.

---
+++ server/server.js
@@ -0,0 +1,66 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import OpenAI from "openai";
+
+
+const app = express();
+app.use(helmet());
+app.use(cors({
+    origin:process.env.FRONTEND_URL || "https://localhost:3000",
+    credentials: true,
+}))
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "Too many requests please try again some time"
+});
+app.use(limiter); 
+app.use(express.json({limit: "10mb"}))
+
+const API_KEY = process.env.NEBIUS_API_KEY
+const client = OpenAI({
+    baseURL: 'https://api.tokenfactory.nebius.com/v1/',
+    apiKey: API_KEY,
+});
+
+
+app.post("/api/code-explainer", async(req,res)=>{
+       try {
+         const {code, language} = req.body; 
+         if (!code){
+            return res.status(400).json({error: "Code is required"})
+         }
+
+
+         const messages = [{
+            role: "user",
+            content: `Please explain this ${language || ""} code in simple terms:\n\n\`\`\`${language || ""}\n${code}\n\`\`\``
+         }]
+
+         const response = await client.chat.completions.create({
+            "model": "openai/gpt-oss-120b",
+            messages,
+            temperature: 0.3,
+            max_tokens: 800,
+         })
+
+         const explaination = response?.choices[0]?.message?.content;
+         if(!explaination){
+            return res.status(500).json({error:"Server Error", details: error.message})
+         }
+
+         res.json({explaination, language:language || "unknown"})
+       } catch (error) {
+        console.error("Code Explainer API Error:", error)
+        res.status(500).json({error:"Server Error:", details: error.message})
+       }
+})
+
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, ()=>{
+    console.log(`API server is listening on https://localhost${PORT}`); 
+})
